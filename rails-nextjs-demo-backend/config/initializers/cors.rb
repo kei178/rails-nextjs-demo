139a88f56a394079090fc879d6605a28b02c3b8f
Kei178 <keisuke.inaba.cpa@gmail.com>
2020-07-13 21:43:42 +1000

    Updated cors setting

--- rails-nextjs-demo-backend/config/initializers/cors.rb
+++ rails-nextjs-demo-backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['localhost', '127.0.0.1']
+    origins ['localhost', '127.0.0.1', 'rails-nextjs-demo-front.vercel.app']
 
     resource '*',
       headers: :any,
